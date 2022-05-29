71a1cbb68e896032b4d91bdc48fefa89c17e9665
Leonardo Dagnino <leodag.sch@gmail.com>
2022-05-28 23:03:37 -0300

    updated consult defaults

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -516,7 +516,8 @@ Akin to `projectile-header-line''s behaviour."
    :preview-key '(:debounce 1 any)
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file consult-xref
-   consult--source-file consult--source-project-file consult--source-bookmark
+   consult--source-bookmark consult--source-recent-file
+   consult--source-project-recent-file
    :preview-key (kbd "M-."))
 
   (setq consult-narrow-key "<")
