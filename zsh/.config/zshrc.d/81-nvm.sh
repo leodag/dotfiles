412a06ae45f174736d273597ac2232e04edb22b3
Leonardo Dagnino <leodag.sch@gmail.com>
2024-03-29 19:58:13 -0300

    don't care if nvm is not installed

--- zsh/.config/zshrc.d/81-nvm.sh
+++ zsh/.config/zshrc.d/81-nvm.sh
@@ -6,6 +6,4 @@ if [[ -f /usr/share/nvm/init-nvm.sh ]]; then
     if [[ ! $(nvm current) == 'system' ]]; then
         echo "NVM not using system as default"
     fi
-else
-    echo "NVM not installed!"
 fi
