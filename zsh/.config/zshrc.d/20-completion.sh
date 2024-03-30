173817ccd685644b6f77690dfea7caa9c9c4ff49
Leonardo Dagnino <leodag.sch@gmail.com>
2024-03-30 09:55:17 -0300

    better completion, hopefully

--- zsh/.config/zshrc.d/20-completion.sh
+++ zsh/.config/zshrc.d/20-completion.sh
@@ -2,29 +2,17 @@
 
 # The following lines were added by compinstall
 
-zstyle ':completion:*' auto-description 'specify: %d'
-zstyle ':completion:*' completer _complete _ignored _match _correct _approximate
-zstyle ':completion:*' expand prefix suffix
 zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' ignore-parents parent pwd
-zstyle ':completion:*' insert-unambiguous true
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' list-prompt %S%p %l%s
 zstyle ':completion:*' list-suffixes true
-zstyle ':completion:*' matcher-list '' '+m:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|[._-]=** r:|=**' 'l:|=* r:|=*'
-zstyle ':completion:*' match-original both
-zstyle ':completion:*' max-errors 8
 zstyle ':completion:*' menu select=1
-zstyle ':completion:*' original true
-zstyle ':completion:*' preserve-prefix '//[^/]##/'
-zstyle ':completion:*' prompt '%e errors'
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' select-prompt %S%p %l%s
 zstyle ':completion:*' verbose true
-zstyle :compinstall filename '/home/leodag/.zshrc'
+zstyle :compinstall filename '/home/leodag/.config/zshrc.d/20-completion.sh'
 
 autoload -Uz compinit
-compinit
 # End of lines added by compinstall
 zstyle ':completion:*' rehash true
 
