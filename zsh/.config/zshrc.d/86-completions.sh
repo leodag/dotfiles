0df36faa0e6c13b783e2b9119963a0209de448bd
Leonardo Dagnino <leodag.sch@gmail.com>
2026-05-20 17:07:20 -0300

    completion updates

--- zsh/.config/zshrc.d/86-completions.sh
+++ zsh/.config/zshrc.d/86-completions.sh
@@ -7,3 +7,14 @@ fi
 if command-exists aws; then
     complete -C /usr/local/bin/aws_completer aws
 fi
+
+if commands-exist mise usage; then
+    source <(mise completion zsh)
+else
+    echo "Binary usage missing!"
+fi
+
+if command-exists rg; then
+    # sed is a workaround for ubuntu without https://github.com/BurntSushi/ripgrep/pull/2957
+    . <(rg --generate complete-zsh | sed 's/_rg "$@"/compdef _rg rg/')
+fi
