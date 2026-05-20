95152a8510262e0d0e147c9f4f4e5f7035966744
Leonardo Dagnino <leodag.sch@gmail.com>
2026-05-20 17:00:54 -0300

    remove nvm and asdf

--- zsh/.config/zshrc.d/80-mise.sh
+++ zsh/.config/zshrc.d/80-mise.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 
-source <(/home/leonardo.dagnino/.local/bin/mise activate zsh)
+if [[ -f /home/leonardo.dagnino/.local/bin/mise ]]; then
+    source <(/home/leonardo.dagnino/.local/bin/mise activate zsh)
+else
+    echo "Mise not installed!"
+fi
--- zsh/.config/zshrc.d/81-nvm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-if [[ -f /usr/share/nvm/init-nvm.sh ]]; then
-    source /usr/share/nvm/init-nvm.sh
-
-    if [[ ! $(nvm current) == 'system' ]]; then
-        echo "NVM not using system as default"
-    fi
-fi
